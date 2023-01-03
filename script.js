bf3ceadc151343043ca7c42d60975d1b45288694
Victor <laivictor80@gmail.com>
2023-01-02 20:08:44 -0800

    update js and css

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let yourScoreText= document.querySelector("#yourScore");
 let computerScoreText = document.querySelector("#computerScore");
-let yourChoiceText = document.querySelector("#yourChoice");
-let computerChoiceText = document.querySelector("#computerChoice");
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -57,51 +55,102 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+function endGame(winner) {
+    const end = document.getElementById("end");
+    const h1 = document.createElement("h1");
+    if(winner=="You") {
+        h1.textContent="you win!";
+    }
+    else if(winner=="Computer") {
+        h1.textContent="you lose!";
+    }
+    const button = document.createElement("btn");
+    button.textContent="Restart";
+    button.style.color="white";
+    button.style.textAlign="center";
+    button.addEventListener("click", ()=> {
+        yourScore=0;
+        computerScore=0;
+        yourScoreText.textContent = "You: " + yourScore;
+        computerScoreText.textContent = "Computer: " + computerScore;
+        h1.remove();
+        button.remove();
+    })
+    end.appendChild(h1);
+    end.appendChild(button);
+}
 
 const rock = document.getElementById("rock");
 rock.addEventListener("click", () => {
-    let computerChoice = getComputerChoice();
-    let result = playRound("rock", computerChoice);
-    if (result==win) {
-        yourScore++;
+    if((yourScore < 5) && (computerScore < 5)) {
+        let computerChoice = getComputerChoice();
+        let result = playRound("rock", computerChoice);
+        if (result==win) {
+            yourScore++;
+        }
+        else if (result==lose) {
+            computerScore++;
+        }
+        yourScoreText.textContent = "You: " + yourScore;
+        computerScoreText.textContent = "Computer: " + computerScore;
+        document.getElementById("yourChoice").src="../images/rock.png";
+        let computerChoicePath= "../images/"+computerChoice+".png";
+        document.getElementById("computerChoice").src=computerChoicePath;
+        if(yourScore==5) {
+            endGame("You");
+        }
+        if(computerScore==5) {
+            endGame("Computer");
+        }
     }
-    else if (result==lose) {
-        computerScore++;
-    }
-    yourScoreText.textContent = "You: " + yourScore;
-    computerScoreText.textContent = "Computer: " + computerScore;
-    yourChoiceText.textContent = "You chose: rock";
-    computerChoiceText.textContent = "Computer chose: " + computerChoice;
 });
 
 const paper = document.getElementById("paper");
 paper.addEventListener("click", () => {
-    let computerChoice = getComputerChoice();
-    let result = playRound("paper", computerChoice);
-    if (result==win) {
-        yourScore++;
+    if((yourScore < 5) && (computerScore < 5)) {
+        let computerChoice = getComputerChoice();
+        let result = playRound("paper", computerChoice);
+        if (result==win) {
+            yourScore++;
+        }
+        else if (result==lose) {
+            computerScore++;
+        }
+        yourScoreText.textContent = "You: " + yourScore;
+        computerScoreText.textContent = "Computer: " + computerScore;
+        document.getElementById("yourChoice").src="../images/paper.png";
+        let computerChoicePath= "../images/"+computerChoice+".png";
+        document.getElementById("computerChoice").src=computerChoicePath;
+        if(yourScore==5) {
+            endGame("You");
+        }
+        if(computerScore==5) {
+            endGame("Computer");
+        }
     }
-    else if (result==lose) {
-        computerScore++;
-    }
-    yourScoreText.textContent = "You: " + yourScore;
-    computerScoreText.textContent = "Computer: " + computerScore;
-    yourChoiceText.textContent = "You chose: paper";
-    computerChoiceText.textContent = "Computer chose: " + computerChoice;
 });
 
 const scissors = document.getElementById("scissors");
 scissors.addEventListener("click", () => {
-    let computerChoice = getComputerChoice();
-    let result = playRound("scissors", computerChoice);
-    if (result==win) {
-        yourScore++;
+    if((yourScore < 5) && (computerScore < 5)) {
+        let computerChoice = getComputerChoice();
+        let result = playRound("scissors", computerChoice);
+        if (result==win) {
+            yourScore++;
+        }
+        else if (result==lose) {
+            computerScore++;
+        }
+        yourScoreText.textContent = "You: " + yourScore;
+        computerScoreText.textContent = "Computer: " + computerScore;
+        document.getElementById("yourChoice").src="../images/scissors.png";
+        let computerChoicePath= "../images/"+computerChoice+".png";
+        document.getElementById("computerChoice").src=computerChoicePath;
+        if(yourScore==5) {
+            endGame("You");
+        }
+        if(computerScore==5) {
+            endGame("Computer");
+        }
     }
-    else if (result==lose) {
-        computerScore++;
-    }
-    yourScoreText.textContent = "You: " + yourScore;
-    computerScoreText.textContent = "Computer: " + computerScore;
-    yourChoiceText.textContent = "You chose: scissors";
-    computerChoiceText.textContent = "Computer chose: " + computerChoice;
 });
